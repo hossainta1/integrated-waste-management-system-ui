252c5ac31d6eb5e3b3003024220f2b210623968b
Tanvir Hossain <tanvir.hossain303030@gmail.com>
2025-02-26 21:28:30 +0600

    Set up Router

---
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+    return (
+        <div>
+            <h2 className="text-2xl">Thhis is Footer</h2>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/Pages/Shared/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+
+
+const NavBar = () => {
+    return (
+        <div>
+            <h2 className="text-2xl">This is Nav</h2>
+        </div>
+    );
+};
+
+export default NavBar;
