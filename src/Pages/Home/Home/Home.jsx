71ab626b94cb9e4ae10d2641942d8f1ef8090998
Tanvir Hossain <tanvir.hossain303030@gmail.com>
2025-02-28 18:58:06 +0600

    Crate Landing page banner

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,7 +1,9 @@
+import Banner from "../Banner/Banner";
 
 const Home = () => {
     return (
         <div>
+            <Banner></Banner>
             <h2 className="text-3xl">This is Home</h2>
         </div>
     );
